879273aa4179fbb9f8dd53a26b7133b5ea270826
jessi ortega <ortegabx05@gmail.com>
2019-07-01 22:08:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,16 +3,7 @@ def speak_to_grandma(phrase)
    puts "HUH?! SPEAK UP, SONNY!"
  elsif 
    puts "NO, NOT SINCE 1938!"
-else
+ else phrase == "I lOVE YOU GRANDMA"
+   puts "I LOVE YOU TOO PUMPKIN!"
+end
  
- 
-
-
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
