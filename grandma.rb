40965cd3baf0239bb6ea8deb63ddcb016b70ed0a
jessi ortega <ortegabx05@gmail.com>
2019-07-01 22:14:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,7 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
     else
   		return "NO, NOT SINCE 1938!"
     end
+end
