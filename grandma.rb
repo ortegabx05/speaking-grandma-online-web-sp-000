839fbd670fdf6c2f97e0980b484d4f25a0716290
jessi ortega <ortegabx05@gmail.com>
2019-07-01 21:59:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
  if phrase == phrase.uppercase
-   puts ""
+   puts "NO, NOT SINCE 1938!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
