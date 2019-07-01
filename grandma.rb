d3f0e9c46268f9663bf4fb7ebed08fea714ea624
jessi ortega <ortegabx05@gmail.com>
2019-07-01 22:12:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,5 @@
-phrase = "Hi Nana, how are you?"
+
 def speak_to_grandma(phrase)
- if phrase != phrase.uppercase
-   puts "HUH?! SPEAK UP, SONNY!"
- elsif 
-   puts "NO, NOT SINCE 1938!"
- else phrase == "I lOVE YOU GRANDMA"
-   puts "I LOVE YOU TOO PUMPKIN!"
- end
-end
+ if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+elsif 
